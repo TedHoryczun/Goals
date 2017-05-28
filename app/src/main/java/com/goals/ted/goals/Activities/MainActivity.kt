d7534af8a0df2c99409fe243ecac78e8a85921de
ted <tedhoryczuniv@outlook.com>
2017-05-27 21:27:55 -0400

    Converted MainActivity to Kotlin

--- app/src/main/java/com/goals/ted/goals/Activities/MainActivity.kt
+++ app/src/main/java/com/goals/ted/goals/Activities/MainActivity.kt
@@ -2,35 +2,23 @@ package com.goals.ted.goals.Activities
 
 import android.content.Intent
 import android.net.Uri
-import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.util.Log
-import android.view.View
-import android.widget.Toast
-
 import com.goals.ted.goals.Fragments.FragmentList
-import com.goals.ted.goals.Goal
 import com.goals.ted.goals.R
-import com.goals.ted.goals.GoalAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.intentFor
 
 class MainActivity : AppCompatActivity(), FragmentList.OnFragmentInteractionListener {
-    private val goal: Goal? = null
-    private val recyclerView: RecyclerView? = null
-    private val adapter: GoalAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.my_toolbar) as Toolbar?
-        setSupportActionBar(toolbar)
-        val fab = findViewById(R.id.fab) as FloatingActionButton?
-        fab!!.setOnClickListener {
-            val intent = Intent(applicationContext, CreateGoalActivity::class.java)
-
-            startActivity(intent)
+        setSupportActionBar(my_toolbar as Toolbar?)
+        fab.setOnClickListener {
+            startActivity(intentFor<CreateGoalActivity>())
             Log.d("onClicked", "fab clicked")
         }
         supportFragmentManager.beginTransaction().replace(R.id.container, FragmentList())
