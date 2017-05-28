d620bd9ea578b7cdc5b6671a99f32d13fee97845
ted <tedhoryczuniv@outlook.com>
2017-05-28 18:55:01 -0400

    GoalDates to Kotlin

---
+++ app/src/main/java/com/goals/ted/goals/GoalDates.kt
@@ -0,0 +1,12 @@
+package com.goals.ted.goals
+
+import java.util.Calendar
+
+/**
+ * Created by ted on 7/20/16.
+ */
+open class GoalDates {
+    var startDate: Calendar? = null
+
+    var endDate: Calendar? = null
+}
